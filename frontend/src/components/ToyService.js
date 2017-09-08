5eb3b2a9904094f26242a060bced6c83f354feaf
Michelle Kidd <michekidd@gmail.com>
2017-09-08 10:53:56 -0500

    frontend create

---
+++ frontend/src/components/ToyService.js
@@ -0,0 +1,17 @@
+//HELPER
+import axios from 'axios';
+
+class ToyService {
+  sendData (data) {
+    axios.post('http://localhost:4200/toys/add/post', { toy:data
+     })
+     .then(function(response) {
+       console.log(response);
+     })
+     .catch(function (err) {
+       console.log(error)
+     });
+  }
+}
+
+export default ToyService;
